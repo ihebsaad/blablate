64d2ce9a3f3e69a609e2249d2b7ca3f9634de8ae
iheb <ihebsaad@gmail.com>
2021-06-08 17:34:33 +0100

    style

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -136,7 +136,7 @@
              </div>	
 				<div class="form-group ">
                     <label for="contenu">Contenu:</label>
-                         <textarea style="min-height: 200px;"  id="contenu" type="text"  class="form-control" placeholder="Votre Question/Suggestion .." name="contenu" required  ></textarea>
+                         <textarea style="min-height: 150px;"  id="contenu" type="text"  class="form-control" placeholder="Votre Question/Suggestion .." name="contenu" required  ></textarea>
  				</div>
  
           <div class="form-group ">
