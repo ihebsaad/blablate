928fce70fd9c164f19a04fc12345d9e212bf45c0
iheb <ihebsaad@gmail.com>
2021-06-24 01:23:48 +0100

    update style emojis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('rgbd','contact','sendmessage');
+        $this->middleware('auth')->except('rgbd','contact','sendmessage','signaler');
     }
 
     /**
