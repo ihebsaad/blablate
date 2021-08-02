7b8b37ca5bc1a0d32a8d997dac0c2ef445ac92c8
ihebsaad <ihebsaad@gmail.com>
2021-08-02 17:47:27 +0100

    couleur salon

--- resources/views/vendor/Chatify/layouts/modals.blade.php
+++ resources/views/vendor/Chatify/layouts/modals.blade.php
@@ -124,8 +124,13 @@
 	   		if($user->sexe=='masculin'){$color='#4167d5';}else{$color='#ec3aa5';}
 
 			$salon=\App\Salon::where('id',$user->salon)->first();
+			$type=$salon->type; 
+			$couleur='';
+			if($type=='public'){$couleur='#68a5ff';}
+			if($type=='vip'){$couleur='#18aa76';}
+			
 		if($user->salon>0){$tot= '('.\App\User::where('salon',$salon['id'])->where('active_status',1)->count().')'; }else{$tot='';}
-			echo '<tr><td style="color:'.$color.'">'.$user->prefixe.' '.$user->username.' ('.$user->age.' ans)</td><td>'.$salon['name'].' '.$tot.'</td></tr>';
+			echo '<tr><td style="color:'.$color.'">'.$user->prefixe.' '.$user->username.' ('.$user->age.' ans)</td><td style="color:'.$couleur.'">'.$salon['name'].' '.$tot.'</td></tr>';
 	   }
 	   ?>
 	   </table>
